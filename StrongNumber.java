73c77e8521d64dc68c1315b2e39e7060f576650d
Aldrsze <Aldrinjariel0@gmail.com>
2026-01-29 21:30:55 +0800

    Cleaning

--- StrongNumber.java
+++ StrongNumber.java
@@ -1,42 +1,36 @@
-public class StrongNumber {
+import java.io.Console;
+
+
+// CHECKS IF THE SUM OF THE FACTORIAL OF ITS DIGITS EQUALS THE GIVEN NUMBER
+public class StrongNumber1 {
     public static void main(String[] args) {
-        int num = 40585;
+        Console con = System.console();        
+
+        int num = Integer.parseInt(con.readLine("Enter a Number: "));
         int sum = 0;
         int originalNum = num;
-        int factorial = 1, factorial1 = 1, factorial2 = 1;        
-        
-        int lastDigit = num % 10;
-        System.out.println(lastDigit);
-        for(int i = 1; i <= lastDigit; i++){
-            factorial = factorial * i;
-        }
+        int Digit = 0;        
 
-        num = num / 10;
-        int secDigit = num % 10;
-        System.out.println(secDigit);
-        for(int i = 1; i <= secDigit; i++){
-            factorial1 = factorial1 * i;
-        }
+        while(num > 0){
             
-        for(int i = 1; i <= firstDigit; i++){
-            factorial2 = factorial2 * i;
-        }
-            System.out.println("------------------");
-            System.out.println(factorial);
-            System.out.println(factorial1);
-            System.out.println(factorial2);
-            
-            System.out.println(originalNum);
+            int factorial = 1;
 
-            sum = factorial + factorial1 + factorial2;
+            Digit = num % 10; // num value % 10 gets the last number
+ 
+            for(int i = 1;i <= Digit; i++ ){
+                factorial *= i;
+            } // END FOR LOOP
+                sum = sum + factorial;
 
-            if (sum == originalNum){
-            
-                System.out.println("STRONG NUMBER!");
-            }else{
-               
-                System.out.println("NOT STRONG NUMBER!");
-            }
+                num = num / 10; // num / 10 to remove the last number
+
+        }// END WHILE LOOP        
+
+        if (sum == originalNum){
+            System.out.println("The number is a STRONG number!");
+        }else{
+            System.out.println("The number is NOT a Strong number!");
+        } // END IF-ELSE
 
     } // END MAIN
 } // END CLASS
