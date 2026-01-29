153bb50f9ddacb06df771d8385fb7c0730570b11
Aldrsze <Aldrinjariel0@gmail.com>
2026-01-29 22:27:40 +0800

    documenting

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,5 +1,6 @@
 import java.io.Console;
 
+// Checks if the sum of each digits with an exponent of how many number is each. ex. 123 = 1^3 + 2^3 + 3^3 = n.
 public class ArmstrongNumber{
     public static void main(String[] args){
         Console con = System.console();
@@ -12,17 +13,17 @@ public class ArmstrongNumber{
         int numLength = String.valueOf(num).length();
 
         while (num > 0){
-            int power = 1;
+            int power = 1; // must be 1 always so it resets
 
-            digit = num % 10;
+            digit = num % 10; // gets the last digit
             
             for(int i = 0; i < digit; i++){
-                power = (int) Math.pow(digit, numLength);
+                power = (int) Math.pow(digit, numLength); // Math.pow is a double so it need to be converted into int (int).
 
 
             }// FOR LOOP   
          
-            num = num / 10; 
+            num = num / 10; // removes the last digit
             sum = sum + power;
 
         } // END WHILE LOOP
